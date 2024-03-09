0b869590da18a32c59c57dd249eb02714ecbc773
lucas_abmael <lucasabmaelbandeira@gmail.com>
2024-03-09 19:31:22 -0300

    agora sim, home page finalizada! :+1:

--- script.js
+++ script.js
@@ -20,14 +20,13 @@ setInterval(() => {
 // menu
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
-var link = document.querySelectorAll(".nav-link");
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
 
-link.addEventListener("click", () => {
-    hamburguer.classList.remove('active');
-    navMenu.classList.remove('active');
-})
+function fecharMenu() {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
