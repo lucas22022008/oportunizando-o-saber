fe2d54062f632e8d4504258f1327d06eca669ea8
lucas_abmael <lucasabmaelbandeira@gmail.com>
2024-03-09 19:14:56 -0300

    pequenos detalhes alterados

--- script.js
+++ script.js
@@ -20,8 +20,14 @@ setInterval(() => {
 // menu
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
+var link = document.querySelectorAll(".nav-link");
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
+})
+
+link.addEventListener("click", () => {
+    hamburguer.classList.remove('active');
+    navMenu.classList.remove('active');
 })
