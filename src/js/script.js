22e34e97c6a37830713028f0b2b2b20bd282dd48
lucas_abmael <lucasabmaelbandeira@gmail.com>
2024-03-10 17:45:56 -0300

    boas praticas, branco suave e pastas.
    criei variáveis para as cores (o que devia estra feito desde o começo) e mudei o branco de fundo da pagina, deixando mais agradável para os olhos e também criei pastas para separar melhor os arquivis.

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+};
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+};
+
+setInterval(() => {
+    next();
+}, 7000);
+
+// menu
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+function fecharMenu() {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
